7989d81d73fc32089c866c7a88a8c344595c0357
Alesia <alesik2000@gmail.com>
2021-04-15 10:26:26 +0300

    app.py modif

--- app.py
+++ app.py
@@ -230,9 +230,9 @@ def serve_layout_local():
 
 
 
-if api_is_up == 'True':
-	app.layout = serve_layout
 if api_is_up == 'False':
+	app.layout = serve_layout
+if api_is_up == 'True':
 	app.layout = serve_layout_local
 
 
